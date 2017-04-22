fe3313a72eae5fb49b6649c6a8b7112284fc16ab
azixMcAze <mathieu.leber@gmail.com>
2017-04-22 14:12:52 +0000

    Created empty CustomPropertyDrawer

---
+++ Assets/Editor/SerializableDictionaryPropertyDrawer.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class SerializableDictionaryPropertyDrawer<TKey, TValue> : PropertyDrawer
+{
+    // public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+    // {
+	// }
+}
+
+[CustomPropertyDrawer(typeof(DictionaryTest.StringDictionary))]
+public class StringDictionaryPropertyDrawer : SerializableDictionaryPropertyDrawer<string, string>
+{
+}

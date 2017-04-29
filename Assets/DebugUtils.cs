0918c36327188c4d8a8f0345678b1410d5379300
azixMcAze <mathieu.leber@gmail.com>
2017-04-29 15:02:32 +0000

    Created static classes with debug print methods

---
+++ Assets/DebugUtils.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using System;
+
+public static class DebugUtils
+{
+	public static string ArrayToString(Array array)
+	{
+		if(array == null)
+			return "null";
+		else
+			return "{" + string.Join(", ", array.Cast<object>().Select(o => o.ToString()).ToArray()) + "}";
+	}
+
+	public static string DictionaryToString<TKey, TValue>(Dictionary<TKey, TValue> dict)
+	{
+		if(dict == null)
+			return "null";
+		else
+			return "{" + string.Join(", ", dict.Select(kvp => kvp.Key.ToString() + ":" + kvp.Value.ToString()).ToArray()) + "}";  
+	}
+}
---
+++ Assets/Editor/DebugUtils.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.Text;
+
+public static class DebugUtils
+{
+	public static string PrintEnumerate(SerializedProperty property)
+	{
+		StringBuilder sb = new StringBuilder();
+		var iterator = property.Copy();
+		var end = property.GetEndProperty();
+		do
+		{
+			sb.AppendLine(iterator.propertyPath + " " + iterator.type + " " + iterator.propertyType.ToString());
+		} while(iterator.Next(true) && iterator.propertyPath != end.propertyPath);
+		return sb.ToString();
+	}
+}

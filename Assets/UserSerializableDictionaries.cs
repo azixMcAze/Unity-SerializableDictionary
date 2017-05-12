46273718ea8ac59fa002dc47855637df071f143a
azixMcAze <mathieu.leber@gmail.com>
2017-05-12 11:12:25 +0200

    Added using directive

--- Assets/UserSerializableDictionaries.cs
+++ Assets/UserSerializableDictionaries.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System;
 using UnityEngine;
 
-[System.Serializable]
+[Serializable]
 public class StringStringDictionary : SerializableDictionary<string, string> {}

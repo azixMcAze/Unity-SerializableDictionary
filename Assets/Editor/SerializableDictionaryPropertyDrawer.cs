883df24df6c2ae166cbc2cab429fad0875194fa8
azixMcAze <mathieu.leber@gmail.com>
2017-05-13 22:17:10 +0200

    Fixed icon tooltips

--- Assets/Editor/SerializableDictionaryPropertyDrawer.cs
+++ Assets/Editor/SerializableDictionaryPropertyDrawer.cs
@@ -10,11 +10,11 @@ public class SerializableDictionaryPropertyDrawer : PropertyDrawer
 	const string KeysFieldName = "m_keys";
 	const string ValuesFieldName = "m_values";
 
-	static GUIContent m_iconPlus = EditorGUIUtility.IconContent ("Toolbar Plus", "|Add entry");
-	static GUIContent m_iconMinus = EditorGUIUtility.IconContent ("Toolbar Minus", "|Remove entry");
-	static GUIContent m_warningIconConflict = EditorGUIUtility.IconContent ("console.warnicon.sml", "|Conflicting key, this entry will be lost");
-	static GUIContent m_warningIconOther = EditorGUIUtility.IconContent ("console.infoicon.sml", "|Conflicting key");
-	static GUIContent m_warningIconNull = EditorGUIUtility.IconContent ("console.warnicon.sml", "|Null key, this entry will be lost");
+	static GUIContent m_iconPlus = IconContent ("Toolbar Plus", "Add entry");
+	static GUIContent m_iconMinus = IconContent ("Toolbar Minus", "Remove entry");
+	static GUIContent m_warningIconConflict = IconContent ("console.warnicon.sml", "Conflicting key, this entry will be lost");
+	static GUIContent m_warningIconOther = IconContent ("console.infoicon.sml", "Conflicting key");
+	static GUIContent m_warningIconNull = IconContent ("console.warnicon.sml", "Null key, this entry will be lost");
 	static GUIStyle m_buttonStyle = GUIStyle.none;
 
 	object m_conflictKey = null;
@@ -275,8 +275,12 @@ public class SerializableDictionaryPropertyDrawer : PropertyDrawer
 			PropertyInfo propertyInfo = serializedPropertyType.GetProperty(kvp.Value, flags);
 			ms_serializedPropertyValueAccessorsDict.Add(kvp.Key, propertyInfo);
 		}
+	}
 
-		m_iconMinus.tooltip = "Remove entry";
+	static GUIContent IconContent(string name, string tooltip)
+	{
+		var builtinIcon = EditorGUIUtility.IconContent (name);
+		return new GUIContent(builtinIcon.image, tooltip);
 	}
 
 	static void DeleteArrayElementAtIndex(SerializedProperty arrayProperty, int index)

9031eb40bc4fe4729686dd982f0d8ac1bbd9f2aa
azixMcAze <mathieu.leber@gmail.com>
2017-05-14 22:03:00 +0200

    Added conditional compilation test to support unity < 5.6

--- Assets/Editor/DebugUtilsEditor.cs
+++ Assets/Editor/DebugUtilsEditor.cs
@@ -13,7 +13,12 @@ public static class DebugUtilsEditor
 		var end = property.GetEndProperty();
 		do
 		{
-			sb.AppendLine(iterator.propertyPath + " (" + iterator.type + " " + iterator.propertyType + ") = " + SerializableDictionaryPropertyDrawer.GetPropertyValue(iterator) + (iterator.isArray ? " (" + iterator.arrayElementType + ")" : ""));
+			sb.AppendLine(iterator.propertyPath + " (" + iterator.type + " " + iterator.propertyType + ") = "
+				+ SerializableDictionaryPropertyDrawer.GetPropertyValue(iterator)
+				#if UNITY_5_6_OR_NEWER
+				+ (iterator.isArray ? " (" + iterator.arrayElementType + ")" : "")
+				#endif
+				);
 		} while(iterator.Next(true) && iterator.propertyPath != end.propertyPath);
 		return sb.ToString();
 	}

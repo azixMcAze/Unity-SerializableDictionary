c350ae6d1112f3786fcbf2b773e92f4479a45532
azixMcAze <mathieu.leber@gmail.com>
2018-01-14 17:58:28 +0100

    Changed SaveProperty method to static

--- Assets/SerializableDictionary/Editor/SerializableDictionaryPropertyDrawer.cs
+++ Assets/SerializableDictionary/Editor/SerializableDictionaryPropertyDrawer.cs
@@ -279,7 +279,7 @@ public class SerializableDictionaryPropertyDrawer : PropertyDrawer
 		}
 	}
 
-	void SaveProperty(SerializedProperty keyProperty, SerializedProperty valueProperty, int index, int otherIndex, ConflictState conflictState)
+	static void SaveProperty(SerializedProperty keyProperty, SerializedProperty valueProperty, int index, int otherIndex, ConflictState conflictState)
 	{
 		conflictState.m_conflictKey = GetPropertyValue(keyProperty);
 		conflictState.m_conflictValue = GetPropertyValue(valueProperty);

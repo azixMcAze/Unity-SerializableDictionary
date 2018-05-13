a29081eceb3823afcda3493f98b6ec11db5c75f5
azixMcAze <mathieu.leber@gmail.com>
2018-05-13 14:35:57 +0200

    Added common base class for SerializableDictionary and SerializableIListDictionary

--- Assets/SerializableDictionary/SerializableDictionary.cs
+++ Assets/SerializableDictionary/SerializableDictionary.cs
@@ -3,18 +3,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SerializableDictionary<TKey, TValue> : Dictionary<TKey, TValue>, ISerializationCallbackReceiver
+public abstract class SerializableDictionaryBase<TKey, TValue, TValueStorage> : Dictionary<TKey, TValue>, ISerializationCallbackReceiver
 {
 	[SerializeField]
 	TKey[] m_keys;
 	[SerializeField]
-	TValue[] m_values;
+	TValueStorage[] m_values;
 
-	public SerializableDictionary()
+	protected abstract void StoreValue(ref TValueStorage storage, TValue value);
+	protected abstract TValue GetValue(TValueStorage storage);
+
+	public SerializableDictionaryBase()
 	{
 	}
 
-	public SerializableDictionary(IDictionary<TKey, TValue> dict) : base(dict.Count)
+	public SerializableDictionaryBase(IDictionary<TKey, TValue> dict) : base(dict.Count)
 	{
 		foreach (var kvp in dict)
 		{
@@ -39,7 +42,7 @@ public class SerializableDictionary<TKey, TValue> : Dictionary<TKey, TValue>, IS
 			int n = m_keys.Length;
 			for(int i = 0; i < n; ++i)
 			{
-				this[m_keys[i]] = m_values[i];
+				this[m_keys[i]] = GetValue(m_values[i]);
 			}
 
 			m_keys = null;
@@ -52,14 +55,35 @@ public class SerializableDictionary<TKey, TValue> : Dictionary<TKey, TValue>, IS
 	{
 		int n = this.Count;
 		m_keys = new TKey[n];
-		m_values = new TValue[n];
+		m_values = new TValueStorage[n];
 
 		int i = 0;
 		foreach(var kvp in this)
 		{
 			m_keys[i] = kvp.Key;
-			m_values[i] = kvp.Value;
+			StoreValue(ref m_values[i], kvp.Value);
 			++i;
 		}
+	}
+}
+
+public class SerializableDictionary<TKey, TValue> : SerializableDictionaryBase<TKey, TValue, TValue>
+{
+    protected override TValue GetValue(TValue storage)
+    {
+        return storage;
+    }
+
+    protected override void StoreValue(ref TValue storage, TValue value)
+    {
+        storage = value;
+    }
+
+	public SerializableDictionary()
+	{
+	}
+
+	public SerializableDictionary(IDictionary<TKey, TValue> dict) : base(dict)
+	{
 	}
 }
